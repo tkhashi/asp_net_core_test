a8792e8ce6c8884d7352f576dcbecca4c54ae641
vagrant <pupodenews@gmail.com>
2021-01-17 10:27:46 +0000

    add partial view

--- SampleMVCApp/SampleMVCApp/Controllers/HelloController.cs
+++ SampleMVCApp/SampleMVCApp/Controllers/HelloController.cs
@@ -11,61 +11,17 @@ namespace SampleMVCApp.Controllers
 {
     public class HelloController : Controller
     {
-        [HttpGet("Hello/{id?}/{name?}")]
-        public IActionResult Index(int id, string name)
-	    {
-        ViewData["message"] = "*セッションにIDとNameを保存しました。";
-	    MyData ob = new MyData(id, name);
-        HttpContext.Session.Set("object", ObjectToBytes(ob));
-	    ViewData["object"] = ob; 
-        return View();
-        }
-
-		[HttpGet]
-	    public IActionResult Other()
-	    {
-		ViewData["message"] = "保存されたセッションの値を表示します。";
-	    byte[] ob = HttpContext.Session.Get("object");
-	    ViewData["object"] = BytesToObject(ob);
-	    return View("index");
-        }
-
-        //convert object to byte[].
-        private byte[] ObjectToBytes(Object ob)
+        [HttpGet]
+        public IActionResult Index()
         {
-            BinaryFormatter bf = new BinaryFormatter();
-            MemoryStream ms = new MemoryStream();
-            bf.Serialize(ms, ob);
-            return ms.ToArray();
-	    }
-
-        //convert byte[] to object.
-        private Object BytesToObject(byte[] arr)
-        {
-            MemoryStream ms = new MemoryStream();
-            BinaryFormatter bf = new BinaryFormatter();
-            ms.Write(arr, 0, arr.Length);
-            ms.Seek(0, SeekOrigin.Begin);
-            return (Object)bf.Deserialize(ms);
-	    }
-    }
-
-    [Serializable]
-    class MyData 
-    {
-        public int Id = 0;
-        public string Name = "";
-
-        public MyData(int id, string name) 
-		{
-            this.Id = id;
-            this.Name = name;
-		}
-
-        public override string ToString()
-        {
-			return "<" + Id + ": " + Name + ">";
+            ViewData["message"] = "*テーブルの表示";
+            ViewData["header"] = new string[] { "id", "name", "mail" };
+            ViewData["data"] = new string[][] {
+                new string[] { "1", "Taro", "taro@yamada" },
+                new string[] { "2", "Hanako", "hanako@flower" },
+                new string[] { "3", "Sachiko", "sachiko@happy" }
+            };
+            return View();
         }
     }
-
 }
