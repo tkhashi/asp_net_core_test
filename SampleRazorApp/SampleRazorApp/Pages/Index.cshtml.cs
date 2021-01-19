dfce58f1d0dc1788cfee7793da01c993f8fa97d0
vagrant <pupodenews@gmail.com>
2021-01-19 08:10:48 +0000

    razar and html

--- SampleRazorApp/SampleRazorApp/Pages/Index.cshtml.cs
+++ SampleRazorApp/SampleRazorApp/Pages/Index.cshtml.cs
@@ -12,28 +12,7 @@ namespace SampleRazorApp.Pages
 {
     public class IndexModel : PageModel
     {
-        public string Message = "no message";
-
-        [DataType(DataType.Text)]
-        public string Name { get; set; }
-
-        [DataType(DataType.Password)]
-        public string Password { get; set; }
-
-        [DataType(DataType.EmailAddress)]
-        public string Mail { get; set; }
-
-        [DataType(DataType.PhoneNumber)]
-        public string Tel { get; set; }
-
-        public void OnGet()
-        {
-            Message = "入力してください";
-        }
-
-        public void OnPost(string name, string password, string mail, string tel)
-        {
-            Message = "[Name: " + name + ", password:(" + password.Length + " chars), mail:" + mail + " <" + tel + ">]";
-        }
+        [BindProperty(SupportsGet = true)]
+        public int Num { get; set; }
     }
 }
